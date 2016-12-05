cc57b7bf531e170c61d53d8b16ec833a58b2443a
da07ng <gaodaojing@gmail.com>
2016-12-05 20:19:46 +0800

    refactor: move initdb into server/init.go

--- main.go
+++ main.go
@@ -2,28 +2,8 @@ package main
 
 import (
 	"github.com/meapex/meapex/server"
-	"github.com/meapex/meapex/server/db"
-	"github.com/meapex/meapex/server/models"
-	"github.com/pelletier/go-toml"
 )
 
 func main() {
-	config, _ := toml.LoadFile("./config/config.toml")
-
-	databaseHost := config.Get("database.host").(string)
-	databaseDBname := config.Get("database.dbname").(string)
-	databaseUsername := config.Get("database.username").(string)
-	databasePassword := config.Get("database.password").(string)
-
-	db.InitDB(databaseHost, databaseUsername, databasePassword, databaseDBname)
-	db.ORM.AutoMigrate(&models.User{})
-
-	redisHost := config.Get("redis.host").(string)
-	redisPort := config.Get("redis.port").(string)
-	redisDBname := config.Get("redis.dbname").(int64)
-	redisPassword := config.Get("redis.password").(string)
-
-	db.InitPool(redisHost, redisPort, redisPassword, redisDBname)
-
 	server.Init()
 }
--- server/init.go
+++ server/init.go
@@ -6,10 +6,16 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/meapex/meapex/server/controllers"
 	"github.com/meapex/meapex/server/controllers/api"
+	"github.com/meapex/meapex/server/db"
+	"github.com/meapex/meapex/server/models"
+	"github.com/pelletier/go-toml"
 )
 
+// Init ...
 func Init() {
 	r := gin.Default()
+
+	initDB()
 
 	account := r.Group("/account")
 	{
@@ -28,4 +34,24 @@ func Init() {
 	}
 
 	r.Run(":8080")
+}
+
+// initDB ...
+func initDB() {
+	config, _ := toml.LoadFile("./config/config.toml")
+
+	databaseHost := config.Get("database.host").(string)
+	databaseDBname := config.Get("database.dbname").(string)
+	databaseUsername := config.Get("database.username").(string)
+	databasePassword := config.Get("database.password").(string)
+
+	db.InitDB(databaseHost, databaseUsername, databasePassword, databaseDBname)
+	db.ORM.AutoMigrate(&models.User{})
+
+	redisHost := config.Get("redis.host").(string)
+	redisPort := config.Get("redis.port").(string)
+	redisDBname := config.Get("redis.dbname").(int64)
+	redisPassword := config.Get("redis.password").(string)
+
+	db.InitRedisPool(redisHost, redisPort, redisPassword, redisDBname)
 }
